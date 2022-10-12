c9dd1ea8e97fa87c206e718d8e7692ee4ee325b5
manuing07 <mllancaleo@gmail.com>
2022-10-12 19:43:01 -0300

    cors

--- index.ts
+++ index.ts
@@ -3,10 +3,12 @@ import defaulRoutes from "./routes/default.routes";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
 import sneakerRoutes from "./routes/sneaker.routes";
+import cors from 'cors'
 
 
 const server = new Server();
 
+server.app.use(cors());
 server.app.use(bodyParser.json());
 server.app.use(bodyParser.urlencoded({extended:true}))
 server.app.use('/',defaulRoutes);
